8663d8e9bcf0f6bf8b61701ed7ef03da300054f1
lapenkovdaniil <daniil.adst@gmail.com>
2019-09-04 20:41:04 +0300

    It's amazing

--- Ls/src/main/java/Ls.java
+++ Ls/src/main/java/Ls.java
@@ -1,24 +1,24 @@
 import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.text.DecimalFormat;
 import java.text.SimpleDateFormat;
 import java.util.*;
 
-
 public class Ls {
     static File item;
-    private static boolean l;
-    private static boolean h;
-    private static boolean r;
-    private static String outputPath = "";
+    private boolean lFlag;
+    private boolean hFlag;
+    private boolean rFlag;
+    private String outputPath = "";
     static TreeMap<String, String> answer = new TreeMap<>();
-    public Ls(boolean l, boolean h, boolean o,String outputPath){
-        this.l = l;
-        this.h = h;
-        this.r = o;
+    public Ls(boolean lFlag, boolean hFlag, boolean rFlag,String outputPath){
+        this.lFlag = lFlag;
+        this.hFlag = hFlag;
+        this.rFlag = rFlag;
         this.outputPath = outputPath;
-
     }
+    static Ls ls = new Ls(false, false, false,"");
 
     public static void switchHumanReadable(StringBuilder str, long lengthItemInBytes) {
         int base = 1024;
@@ -26,7 +26,7 @@ public class Ls {
         DecimalFormat lengthItem = new DecimalFormat("###.##");
         int i;
         double k = lengthItemInBytes;
-        for (i = 0; i<2; i++) {
+        for (i = 0; i < 2; i++) {
             if (k >= base) {
                 k /= base;
                 i++;
@@ -51,34 +51,90 @@ public class Ls {
         for (File testFile : array) {
             StringBuilder str = new StringBuilder();
             String lastChange = new SimpleDateFormat("hh:mm").format(new Date(testFile.lastModified()));
-            if (testFile.canRead())
-                    str.append("r");
-            else
-                str.append("-");
-            if (testFile.canWrite())
-                    str.append("w");
-                else
-                        str.append("-");
-            if (testFile.canExecute())
-                        str.append("x");
-                else str.append("-");
+            str.append(testFile.canRead() ? "r" : "-");
+            str.append(testFile.canWrite() ? "w" : "-");
+            str.append(testFile.canExecute() ? "x" : "-");
             str.append("  ").append(lastChange).append("  ");
             long lengthItemInBytes = 0;
-            if (testFile.isFile())
+            if (testFile.isFile()) {
                 lengthItemInBytes = testFile.length();
-            if (mode)
+            }
+            if (mode) {
                 switchHumanReadable(str, lengthItemInBytes);
-            else
+            } else {
                 str.append(lengthItemInBytes);
+            }
 
             answer.replace(testFile.getName(), str.toString());
         }
     }
+    private static void printAnswer() {
+        try
+        {
+            FileWriter outputFile = new FileWriter(ls.outputPath);
 
+            for (Map.Entry<String, String> map : answer.entrySet())
+                if (ls.lFlag)
+                    outputFile.write(map.getValue() + "   " + map.getKey() + "\n");
+                else
+                    outputFile.write(map.getKey() + "\n");
 
-    public static class MyException extends RuntimeException {
-        MyException(String message) {
-            super(message);
+            outputFile.close();
         }
+        catch (IOException e)
+        {
+            System.err.println(e.getMessage());
+        }
+    }
+
+    public static void main(String[] args) {
+        if (args.length == 0) {
+            System.out.println("Ls [-l] [-h] [-r] [-o output.file] directory_or_file");
+            return;
+        }
+        Ls.item = new File(args[args.length - 1]);
+        if (Ls.item.isDirectory()) {
+
+            String[] names = Ls.item.list();
+            if (names.length == 0) {
+                Ls.answer.put("Пусто", "");
+                return;
+            }
+
+            for (String str : names) {
+                Ls.answer.put(str, "");
+            }
+        } else {
+            Ls.answer.put(Ls.item.getName(), "");
+        }
+
+        final int last = args.length - 1;
+        int i = 0;
+        while(i < last) {
+            if (args[i].contains("-l")) {
+                ls.lFlag = true;
+                Ls.getRWX(false);
+                i++;
+                continue;
+
+            }
+            if (args[i].contains("-h")) {
+                ls.hFlag = true;
+                Ls.getRWX(true);
+                i++;
+                continue;
+
+            }
+            if (args[i].contains("-r")) {
+                ls.rFlag = true;
+                Ls.reverseTreeMap(Ls.answer);
+
+            }
+            if (args[i].equals("-o")) {
+                ls.outputPath = args[i++].substring(0, args[i].length() - 1);
+            }
+            return;
+        }
+        Ls.printAnswer();
     }
 }
--- Ls/src/test/java/Tests.java
+++ Ls/src/test/java/Tests.java
@@ -1,33 +1,22 @@
 
-import org.junit.Before;
-import org.junit.Rule;
-import org.junit.contrib.java.lang.system.ExpectedSystemExit;
 import org.junit.jupiter.api.Test;
 import static junit.framework.Assert.assertEquals;
 import java.io.*;
 import java.util.*;
 public class Tests {
-    private ByteArrayOutputStream outContent = new ByteArrayOutputStream();
-    TreeMap<String, String> rightAnswer = new TreeMap<>();
-
-//    @Before
-//    public void setup(){
-//
-//        System.setOut(new PrintStream(outContent));
-//    }
-
+    private TreeMap<String, String> rightAnswer = new TreeMap<>();
     @Test
-    public void testLong() throws IOException {
-        String[] args = {"[-l]", "/C:/Users/User/LowSkill"};
-        Parser.main(args);
+    public void testLong() throws IOException{
+        String[] args = {"-l", "/C:/Users/User/LowSkill"};
+        Ls.main(args);
         rightAnswer.put("RealLs.txt", "rwx  08:19  3855332" );
         rightAnswer.put("1.png", "rwx  07:09  367048");
         assertEquals(rightAnswer, Ls.answer);
     }
     @Test
-    public void testAll() throws IOException {
-        String[] args = {"[-l]", "[-h]", "[-r]", "/C:/Users/User/LowSkill"};
-        Parser.main(args);
+    public void testAll()throws IOException{
+        String[] args = {"-l", "-h", "-r", "/C:/Users/User/LowSkill"};
+        Ls.main(args);
         rightAnswer.put("RealLs.txt", "rwx  08:19  3,68 MB");
         rightAnswer.put("1.png", "rwx  07:09  358,45 KB");
         assertEquals(rightAnswer, Ls.answer);
@@ -35,17 +24,17 @@ public class Tests {
     }
 
     @Test
-    public void testEmpty() throws IOException {
-        String[] args = {"[-l]", "[-h]", "[-r]", "/C:/Users/User/HightSkill"};
-        Parser.main(args);
+    public void testEmpty() throws IOException{
+        String[] args = {"-l", "-h", "-r", "/C:/Users/User/ff"};
+        Ls.main(args);
         rightAnswer.put("Пусто","");
         assertEquals(rightAnswer, Ls.answer);
 
     }
     @Test
     public void testR() throws IOException {
-        String[] args = { "[-r]", "/C:/Users/User/LowSkill"};
-        Parser.main(args);
+        String[] args = { "-l","-r", "/C:/Users/User/LowSkill"};
+        Ls.main(args);
         rightAnswer.put("RealLs.txt", "rwx  08:19  3855332" );
         rightAnswer.put("1.png", "rwx  07:09  367048");
         assertEquals(rightAnswer, Ls.answer);
@@ -53,18 +42,17 @@ public class Tests {
     }
 
     @Test
-    public void testLongHumRead() throws IOException {
-        String[] args = {"[-l]", "[-h]",  "/C:/Users/User/LowSkill"};
-        Parser.main(args);
-        rightAnswer.put("RealLs.txt", "rwx  08:19  3,68 MB");
+    public void testLongHumRead()throws IOException{
+        String[] args = {"-l", "-h","r" ,  "/C:/Users/User/LowSkill"};
+        Ls.main(args);        rightAnswer.put("RealLs.txt", "rwx  08:19  3,68 MB");
         rightAnswer.put("1.png", "rwx  07:09  358,45 KB");
         assertEquals(rightAnswer, Ls.answer);
     }
 
     @Test
-    public void testOutput() throws IOException {
-        String[] args = {"[-l]" ,"[-o]", "example.txt]", "/C:/Users/User/LowSkill/1.png"};
-        Parser.main(args);
+    public void testOutput() throws IllegalArgumentException, IOException {
+        String[] args = {"-o", "example.txt", "example.txt", "/C:/Users/User/LowSkill/1.png"};
+        Ls.main(args);
         String rightAnswer = "rwx  07:09  367048   1.png";
         String fromFile = "";
         try {
